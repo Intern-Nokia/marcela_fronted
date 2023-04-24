fc86442ee1e7f8cf802add0e224726eee31b54fe
jusruizsa <sruizs.jr@gmail.com>
2023-04-24 16:18:45 -0500

    Add navigation to menu navigate

--- src/App.js
+++ src/App.js
@@ -1,32 +1,47 @@
 import "./App.css";
 import { Menu } from "antd";
 import { UserOutlined, BookOutlined, BulbOutlined } from "@ant-design/icons";
+import { Outlet, useNavigate } from "react-router-dom";
 
 function App() {
+
+  const navigate = useNavigate()
+
   return (
-    <Menu mode="horizontal" className="menu">
-      <Menu.Item
-        className="menu-item"
-        key="employees"
-        icon={<UserOutlined style={{fontSize: '32px'}} />}
-      >
-        Empleados
-      </Menu.Item>
-      <Menu.Item
-        className="menu-item"
-        key="courses"
-        icon={<BookOutlined style={{fontSize: '32px'}}/>}
-      >
-        Cursos
-      </Menu.Item>
-      <Menu.Item
-        className="menu-item"
-        key="trainings"
-        icon={<BulbOutlined style={{fontSize: '32px'}} />}
-      >
-        Capacitaciones
-      </Menu.Item>
-    </Menu>
+    <>
+    <div>
+      <Menu mode="horizontal" className="menu">
+        <Menu.Item
+          onClick={() => navigate('employees')}
+          className="menu-item"
+          key="employees"
+          icon={<UserOutlined style={{fontSize: '32px'}} />}
+        >
+          Empleados
+        </Menu.Item>
+        <Menu.Item
+          onClick={() => navigate('courses')}
+          className="menu-item"
+          key="courses"
+          icon={<BookOutlined style={{fontSize: '32px'}}/>}
+        >
+          Cursos
+        </Menu.Item>
+        <Menu.Item
+          onClick={() => navigate('training')}
+          className="menu-item"
+          key="trainings"
+          icon={<BulbOutlined style={{fontSize: '32px'}} />}
+        >
+          Capacitaciones
+        </Menu.Item>
+      </Menu>
+    </div>
+    <div>
+      <Outlet></Outlet>
+    </div>
+    </>
+    
   );
 }
 
